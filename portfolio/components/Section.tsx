8d6cb6b4b351400a8a37089391cf51fce66dcaa8
Anushil98 <anushilghoshdastidar@gmail.com>
2021-04-24 15:49:43 +0530

    DesignRefactor

--- portfolio/components/Section.tsx
+++ portfolio/components/Section.tsx
@@ -2,73 +2,66 @@ import { AnimatePresence, motion } from 'framer-motion';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-export default function Section() {
-	const [ currentTech, setcurrentTech ] = useState(-1);
-	const SubSection = styled.div`
-		height: fit-content;
-		width: 100%;
-		/* display: grid;
-	grid-template-columns: 0.5fr 2fr; */
-		display: flex;
-		flex-direction: column;
-	`;
+const SubSection = styled.div`
+	height: fit-content;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+`;
 
-	const SubHeading = styled.div`
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-		font-size: 5.2vw;
-		font-family: cursive;
-		height: fit-content;
-		margin-bottom: 15px;
-		/* position: absolute; */
-		/* bottom: 0; */
-	`;
+const SubHeading = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	font-size: 5.2vw;
+	font-family: cursive;
+	height: fit-content;
+	margin-bottom: 15px;
+`;
 
-	const CardWrapper = styled.div`
-		display: flex;
-		align-items: center;
-		flex-direction: row;
-		justify-content: space-between;
+const CardWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: flex-start;
+`;
 
-		/* overscroll-behavior: contain; */
-	`;
+const CardWrapperLevel1 = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+`;
 
-	const Card =
-		styled.div <
-		{ grayscale: boolean } >
-		`
+const Card =
+	styled.div <
+	{ grayscale: boolean } >
+	`
 	filter: ${(props) => {
 		return props.grayscale ? 'grayscale(100%)' : 'grayscale(0%)';
 	}} `;
 
-	const SubCard =
-		styled.div <
-		{ bgColor: string, index: number, numElements: number } >
-		`background-color: ${(props) => {
-			return '#' + props.bgColor;
-		}};
-	position:absolute;
+const SubCard =
+	styled.div <
+	{ bgColor: string, numElements: number } >
+	`background-color: ${(props) => {
+		return '#' + props.bgColor;
+	}};
 	
 	width:${(props) => {
-		return props.numElements * 100 + '%';
+		return 100 + '%';
 	}};
-
 	height:auto;
-
-	left:${(props) => {
-		return '-' + props.index * 100 + '%';
-	}};
-
-	// overflow-x:auto;
-	// overflow-y:hidden;
 	 `;
+
+export default function Section() {
+	const [ currentTech, setcurrentTech ] = useState(0);
 
 	const UniqueTechStacks = [
 		{ name: 'typescript', color: '007ACC' },
 		{ name: 'javascript', color: 'F7DF1E' },
 		{ name: 'python', color: '14354C' },
-		{ name: 'javascript', color: 'F7DF1E' }
+		{ name: 'java', color: 'ED8B00' },
+		{ name: 'python', color: '14354C' }
 	];
 	return (
 		<div>
@@ -78,52 +71,49 @@ export default function Section() {
 					backgroundColor: currentTech >= 0 ? '#' + UniqueTechStacks[currentTech].color : '#ffffff'
 				}}
 			>
-				<CardWrapper>
-					{UniqueTechStacks.map((uniq, index) => {
-						return (
-							<motion.div
-								onHoverStart={() => {
-									setcurrentTech(index);
-								}}
-								onHoverEnd={() => {
-									setcurrentTech(-1);
-								}}
-								key={index}
-							>
-								<Card
-									onClick={(e) => {
-										if (currentTech === -1 || currentTech !== index) setcurrentTech(index);
-										else setcurrentTech(-1);
-									}}
-									grayscale={currentTech === index ? false : true}
-								>
-									<img src={`/TechIcon/${uniq.name}.svg`} />
-									{currentTech === index ? (
-										<AnimatePresence>
-											<motion.div
-												initial={{ scaleY: 0 }}
-												animate={{ scaleY: 1 }}
-												transition={{ duration: 0.5 }}
-												exit={{ opacity: 0 }}
-											>
-												<SubCard
-													bgColor={UniqueTechStacks[currentTech].color}
-													index={index}
-													numElements={UniqueTechStacks.length}
-												>
-													<img
-														src={`/LanguageCodeSnippets/${UniqueTechStacks[currentTech]
-															.name}.svg`}
-													/>
-												</SubCard>
-											</motion.div>
-										</AnimatePresence>
-									) : null}
-								</Card>
+				<motion.div
+					onHoverEnd={() => {
+						setcurrentTech(-1);
+					}}
+				>
+					<CardWrapper>
+						<CardWrapperLevel1>
+							{UniqueTechStacks.map((uniq, index) => {
+								return (
+									<motion.div
+										onHoverStart={() => {
+											setcurrentTech(index);
+										}}
+										key={index}
+									>
+										<Card
+											onClick={(e) => {
+												if (currentTech === -1 || currentTech !== index) setcurrentTech(index);
+												else setcurrentTech(-1);
+											}}
+											grayscale={currentTech === index ? false : true}
+										>
+											<img src={`/TechIcon/${uniq.name}.svg`} />
+										</Card>
+									</motion.div>
+								);
+							})}
+						</CardWrapperLevel1>
+
+						<AnimatePresence>
+							<motion.div initial={{ scaleY: 0 }} animate={{ scaleY: 1 }} transition={{ duration: 0.5 }}>
+								{currentTech >= 0 ? (
+									<SubCard
+										bgColor={UniqueTechStacks[currentTech].color}
+										numElements={UniqueTechStacks.length}
+									>
+										<img src={`/LanguageCodeSnippets/${UniqueTechStacks[currentTech].name}.svg`} />
+									</SubCard>
+								) : null}
 							</motion.div>
-						);
-					})}
-				</CardWrapper>
+						</AnimatePresence>
+					</CardWrapper>
+				</motion.div>
 			</SubSection>
 		</div>
 	);
